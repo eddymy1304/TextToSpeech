46d0d8b677c9711acfc28c5e5995b8f6a3c3aeab
Eddy Mendoza <eddy.mendoza@osf.pe>
2023-11-30 11:08:29 -0500

    se cambio el nombre del directorio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.texttospeech"
+    namespace = "com.eddymy1304.texttospeech"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.texttospeech"
+        applicationId = "com.eddymy1304.texttospeech"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
